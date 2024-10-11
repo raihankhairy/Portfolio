5dcde3cffef8aad026244bce1226120f955eb6c0
raihankhairy <raihankhairy21@gmail.com>
2024-10-11 18:20:26 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+
+// Clear from before unload 
+window.onbeforeunload = () => {
+    for (const from of document.getElementsByTagName("form")) {
+        from.reset();
+    }
+};
